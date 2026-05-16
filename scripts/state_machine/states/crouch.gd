6fdaca670492dd938bae939d8d938eb792dc427c
kanashiii111 <FrageSK@yandex.ru>
2026-05-16 20:14:06 +0300

    fixed crouch once again, some essential imports

--- scripts/state_machine/states/crouch.gd
+++ scripts/state_machine/states/crouch.gd
@@ -1,6 +1,6 @@
 class_name PlayerCrouchState extends PlayerState
 
-var need_no_uncrouch: bool = false
+var want_to_uncrouch: bool = false
 
 func _ready() -> void:
 	pass 
@@ -14,27 +14,37 @@ func init():
 func enter():
 	player.player_collider.shape.size.y = player.SLIDE_SHAPE_SIZE_Y
 	player.player_collider.position.y = player.SLIDE_POSITION_Y
+	
+	if state_machine.prev_state.name == "Slide" and not Input.is_action_pressed("Slide"):
+		want_to_uncrouch = true
+	else:
+		want_to_uncrouch = false
 
 func exit():
 	player.sprite.rotation = 0
 	player.player_collider.shape.size.y = player.AFTER_SLIDE_SHAPE_SIZE_Y
 	player.player_collider.position.y = player.AFTER_SLIDE_POSITION_Y
 
-func handle_input(_event: InputEvent) -> PlayerState: 
-	#if not _event.is_action_pressed("Slide"):
-		#return idle
-	if state_machine.prev_state.name == "Slide" and not player.is_ceiling_above():
-		return idle
-	if not player.is_ceiling_above() and need_no_uncrouch:
-		need_no_uncrouch = false
-		return idle
+func handle_input(_event: InputEvent) -> PlayerState:
 	if _event.is_action_released("Slide"):
-		if player.is_ceiling_above():
-			need_no_uncrouch = true
-			return null
-		else:
-			return idle
+		want_to_uncrouch = true
+	
+	if _event.is_action_pressed("Slide"):
+		want_to_uncrouch = false
+	
 	return null
+	#if _event.is_action_released("Slide"):
+		#if player.is_ceiling_above():
+			#need_no_uncrouch = true
+			#return null
+		#else:
+			#return idle
+	#if state_machine.prev_state.name == "Slide" and not player.is_ceiling_above():
+		#return idle
+	#if not player.is_ceiling_above() and need_no_uncrouch:
+		#need_no_uncrouch = false
+		#return idle
+	#return null
 
 func process(_delta: float) -> PlayerState:
 	return null
@@ -42,6 +52,10 @@ func process(_delta: float) -> PlayerState:
 func physics_process(_delta: float) -> PlayerState:
 	player.update_animation_direction()
 	player.update_animation_rotation()
+	
+	if want_to_uncrouch and not player.is_ceiling_above():
+		want_to_uncrouch = false
+		return idle
 	
 	var target_speed = direction.x * 100
 	player.update_velocity( target_speed, 20 )
